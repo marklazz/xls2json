16e9c835f8ae1bf979e87aa166e12f5a3555fca0
Marcelo Giorgi <marklazz.uy@gmail.com>
2011-12-10 13:00:30 -0200

    Move some lib deps to be dev deps

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "Converts Excel files (.xls and .xlsx) to plain JSON data"
   :url "http://github.com/marklazz/xls2json"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [vimclojure/server "2.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [cheshire "2.0.4"]
                  [dk.ative/docjure "1.5.0"]
-                 ])
+                 ]
+  :dev-dependencies [[vimclojure/server "2.2.0"]
+                     [org.clojars.ibdknox/lein-nailgun "1.1.1"]
+                     ])
