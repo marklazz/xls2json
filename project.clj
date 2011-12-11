107ee9abbffd4aeb1371a1476599202e44feea16
Marcelo Giorgi <marklazz.uy@gmail.com>
2011-12-11 16:57:31 -0200

    Upgrade to Clojure 1.2.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject xls2json "1.0.1"
   :description "Converts Excel files (.xls and .xlsx) to plain JSON data"
   :url "http://github.com/marklazz/xls2json"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [cheshire "2.0.4"]
                  [dk.ative/docjure "1.5.0"]
